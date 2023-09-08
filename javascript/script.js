70da7a0038ab790fba8d7baf6a6bcf5da023fdef
Liandermans <139888264+Liandermans@users.noreply.github.com>
2023-09-08 13:22:24 +0200

    Start forecast tomorrow

--- javascript/script.js
+++ javascript/script.js
@@ -45,7 +45,7 @@ function showForecast(response) {
   let forecastHTML = "";
 
   forecast.forEach(function (forecastDay, index) {
-    if (index < 6) {
+    if (index > 0) {
       let iconURL = forecastDay.condition.icon_url;
       let iconDescription = forecastDay.condition.description;
       let tempMax = Math.round(forecastDay.temperature.maximum);
